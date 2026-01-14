5124870004a29e15d56d98df1c66dac5a1626772
Qualy-Inc <thantnyinyisoe.qualymm@gmail.com>
2026-01-14 10:59:08 +0630

    new branch wo

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,16 +1,16 @@
 const $burger = $(".burger");
 if ($burger.length) {
-	const tl1 = gsap.timeline();
-	tl1.to(".top_stack", { y: 7, duration: 0.5, ease: "power2.out" })
-	   .to(".top_stack", { rotation: 45, duration: 0.5, ease: "power2.out" });
+	const tl1 = gsap.timeline({paused: true });
+	tl1.to(".top_stack", { y: 7, duration: 0.3, ease: "power2.out" })
+	   .to(".top_stack", { rotation: 45, duration: 0.3, ease: "power2.out" });
 	   
-	const tl2 = gsap.timeline();
-	tl2.to(".mid_stack", { y: 0, duration: 0.5, ease: "power2.out" })
-		.to(".mid_stack", { rotation: 45, duration: 0.5, ease: "power2.out" });
+	const tl2 = gsap.timeline({paused: true });
+	tl2.to(".mid_stack", { y: 0, duration: 0.3, ease: "power2.out" })
+		.to(".mid_stack", { rotation: 45, duration: 0.3, ease: "power2.out" });
 	
-	const tl3 = gsap.timeline();
-	tl3.to(".bot_stack", { y: -7, duration: 0.5, ease: "power2.out" }, "<")
-	   .to(".bot_stack", { rotation: -45, duration: 0.5, ease: "power2.out" });
+	const tl3 = gsap.timeline({paused: true });
+	tl3.to(".bot_stack", { y: -7, duration: 0.3, ease: "power2.out" }, "<")
+	   .to(".bot_stack", { rotation: -45, duration: 0.3, ease: "power2.out" });
 
 	let isExit = false;
 	$burger.on("click", function () {
